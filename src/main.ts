5b9b32f2f08b8758e6c301515e28bedacfdcd705
r253hmdryou <r253.hmdryou@gmail.com>
2024-01-18 06:49:35 +0900

    mermaidとswaggerの出力を関数化

--- src/main.ts
+++ src/main.ts
@@ -6,42 +6,12 @@ import { dump } from 'js-yaml';
 
 import { createApp } from './app';
 
-import 'share/models/associations';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await createApp();
-  // nestjs-spelunkerの出力をmermaid形式に変換する。
-  {
-    const tree = SpelunkerModule.explore(app);
-    const root = SpelunkerModule.graph(tree);
-    const edges = SpelunkerModule.findGraphEdges(root);
-    console.log('graph LR');
-    const mermaidEdges = edges
-      .map(({ from, to }) => `  ${from.module.name}-->${to.module.name}`)
-      .filter((value) => !value.endsWith('-->SequelizeCoreModule'))
-      .filter((value) => !value.endsWith('-->SequelizeModule')); // いらないはず
-    console.log(mermaidEdges.join('\n'));
-  }
-  // swaggerの出力
-  {
-    const config = new DocumentBuilder()
-      .setTitle('SC API')
-      .setDescription('SC API')
-      .setVersion('1.0')
-      .addGlobalParameters({
-        name: 'X-Requested-With',
-        in: 'header',
-        required: false,
-        // 本来はtrueにすべきだが、クライアント自動生成時に入力を強制されて面倒なのでfalseにしている。
-        // 現状はaxiosのデフォルト値を使っているため、リクエストごとに明示的に指定する必要が無い。
-      })
-      .addBearerAuth()
-      .build();
-    patchNestJsSwagger();
-    const document = SwaggerModule.createDocument(app, config);
-    await fs.promises.writeFile('./dist/swagger-spec.yaml', dump(document));
-    SwaggerModule.setup('api', app, document);
-  }
+  generateMermaidEdges(app);
+  await generateSwagger(app);
 
   app.enableCors({
     origin: process.env.CORS_ORIGIN
@@ -53,3 +23,38 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+function generateMermaidEdges(app: INestApplication): void {
+  // nestjs-spelunkerの出力をmermaid形式に変換する。
+  const tree = SpelunkerModule.explore(app);
+  const root = SpelunkerModule.graph(tree);
+  const edges = SpelunkerModule.findGraphEdges(root);
+  console.log('```');
+  console.log('graph LR');
+  const mermaidEdges = edges
+    .map(({ from, to }) => `  ${from.module.name}-->${to.module.name}`)
+    .filter((value) => !value.endsWith('-->SequelizeCoreModule'))
+    .filter((value) => !value.endsWith('-->SequelizeModule')); // いらないはず
+  console.log(mermaidEdges.join('\n'));
+  console.log('```');
+}
+
+async function generateSwagger(app: INestApplication): Promise<void> {
+  const config = new DocumentBuilder()
+    .setTitle('SC API')
+    .setDescription('SC API')
+    .setVersion('1.0')
+    .addGlobalParameters({
+      name: 'X-Requested-With',
+      in: 'header',
+      required: false,
+      // 本来はtrueにすべきだが、クライアント自動生成時に入力を強制されて面倒なのでfalseにしている。
+      // 現状はaxiosのデフォルト値を使っているため、リクエストごとに明示的に指定する必要が無い。
+    })
+    .addBearerAuth()
+    .build();
+  patchNestJsSwagger();
+  const document = SwaggerModule.createDocument(app, config);
+  await fs.promises.writeFile('./dist/swagger-spec.yaml', dump(document));
+  SwaggerModule.setup('api', app, document);
+}
