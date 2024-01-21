84b76dd5be0fd0149ff80760b4943e253df518c4
HAMADA Ryo <r253.hmdryou@gmail.com>
2024-01-21 23:32:25 +0900

    Added JWT functionality to user registration module and service (#8)

--- src/server/user-registration/user-registration.module.ts
+++ src/server/user-registration/user-registration.module.ts
@@ -5,10 +5,18 @@ import { UserRegistrationRepository } from './user-registration.repository';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserModel } from 'share/models/user.model';
 import { BcryptModule } from 'vendors/bcrypt/bcrypt.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [UserRegistrationController],
-  imports: [SequelizeModule.forFeature([UserModel]), BcryptModule],
+  imports: [
+    SequelizeModule.forFeature([UserModel]),
+    BcryptModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [UserRegistrationService, UserRegistrationRepository],
 })
 export class UserRegistrationModule {}
--- src/server/user-registration/user-registration.service.ts
+++ src/server/user-registration/user-registration.service.ts
@@ -1,37 +1,47 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRegistrationRepository } from './user-registration.repository';
 import { BcryptService } from 'vendors/bcrypt/bcrypt.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserRegistrationService {
   constructor(
     private readonly repository: UserRegistrationRepository,
     private bcrypt: BcryptService,
+    private jwtService: JwtService,
   ) {}
   async create(email: string): Promise<void> {
-    // TODO: generate JWT and send email with confirmation link
+    const token = await this.jwtService.signAsync({
+      email: email,
+    });
+    // TODO: send email with confirmation link
     console.log(`send email to ${email} with confirmation link`);
+    console.log(token);
     return;
   }
 
-  async confirm(token: string): Promise<{ message: string }> {
-    // TODO: validate JWT
-    if (token === 'invalid') {
-      throw new UnauthorizedException();
-    }
-    return { message: 'confirmed' };
+  async confirm(token: string): Promise<{ message: string; email: string }> {
+    const payload = this.verifyToken(token);
+    return { message: 'confirmed', email: payload.email };
   }
 
   async setPassword(token: string, password: string): Promise<void> {
-    // TODO: validate JWT
-    const email = token;
+    const payload = this.verifyToken(token);
     const passwordHash = await this.bcrypt.hash(password, 10);
 
     // TODO: factory method
     const user = {
-      email,
+      email: payload.email,
       passwordHash,
     };
     this.repository.save(user);
+  }
+
+  private verifyToken(token: string) {
+    try {
+      return this.jwtService.verify(token);
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
   }
 }
