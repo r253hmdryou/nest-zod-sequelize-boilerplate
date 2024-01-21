ce1fbc70fd4df128b79686106fe3dfe14d756ae3
HAMADA Ryo <r253.hmdryou@gmail.com>
2024-01-22 00:21:17 +0900

    Added global exception filter and integrated it with the application (#10)
    * Added global exception filter and integrated it with the application
    
    * Refactor getLoggerOption function to accept environment variable as parameter

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,14 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { INestApplication } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from 'share/filters/exception.filter';
 
 export async function createApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule, {
-    logger: process.env.APP_LOGGER as unknown as false,
+    logger: getLoggerOption(process.env.APP_LOGGER),
   });
 
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.setGlobalPrefix('/v1');
 
   return app;
+}
+
+function getLoggerOption(env: string | undefined) {
+  if (env === undefined) {
+    return undefined;
+  }
+  if (env === 'false') {
+    return false;
+  }
+  return undefined;
 }
